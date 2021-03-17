91eb0e47e7d1289202ec0e7d03ccf90f5935c3c9
HarshKhandeparkar <mjkgoa@gmail.com>
2021-03-17 20:29:53 +0530

    fix: temporarily leave rainbow brush tool

--- src/renderers/RealDrawBoard/tools/tools.ts
+++ src/renderers/RealDrawBoard/tools/tools.ts
@@ -1,21 +1,21 @@
 import * as brush from './brush';
 import * as eraser from './eraser';
 import * as line from './line';
-import * as rainbow_brush from './rainbow_brush';
+// import * as rainbow_brush from './rainbow_brush';
 
 export const tools = {
   brush,
-  rainbow_brush,
+  // rainbow_brush,
   eraser,
   line
 }
 
-export type Tool = 'brush' | 'rainbow_brush' | 'eraser' | 'line';
+export type Tool = 'brush' | 'eraser' | 'line';
 
-export type ToolSettings = brush.BrushSettings & eraser.EraserSettings & line.LineSettings & rainbow_brush.RainbowBrushSettings;
+export type ToolSettings = brush.BrushSettings & eraser.EraserSettings & line.LineSettings;
 export const ToolDefaults: ToolSettings = {
   ...brush.BrushDefaults,
   ...line.LineDefaults,
   ...eraser.EraserDefaults,
-  ...rainbow_brush.RainbowBrushDefaults
+  // ...rainbow_brush.RainbowBrushDefaults
 }
