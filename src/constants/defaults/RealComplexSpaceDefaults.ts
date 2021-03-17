599c60e49bda2407ab908f972f91bd1e92bf16af
HarshKhandeparkar <mjkgoa@gmail.com>
2021-03-17 17:52:30 +0530

    feat: remove other real renderers to be done later

--- src/constants/defaults/RealComplexSpaceDefaults.ts
+++
@@ -1,9 +0,0 @@
-import { RealComplexSpaceOptions, WatchedNumbers } from '../../types/RealComplexSpaceTypes';
-
-export const RealComplexSpaceDefaults: RealComplexSpaceOptions = {
-  brushSize: 1,
-  brushColor: [1, 1, 1],
-  changeNumbers: (watchedNumbers: WatchedNumbers, time: number, timeStep: number) => watchedNumbers,
-  lineThickness: 0.5,
-  lineColor: [1, 1, 1]
-}
