69f648f50f1eb1b66e3705dc902af21546aa3fdc
HarshKhandeparkar <mjkgoa@gmail.com>
2021-03-17 22:09:34 +0530

    fix: resize doesn't break svg

--- src/renderers/RealDrawBoard/_coords.ts
+++ src/renderers/RealDrawBoard/_coords.ts
@@ -4,8 +4,8 @@ export function _getMouseCoords(
   this: RealDrawBoard,
   e: MouseEvent
 ): [number, number] {
-  const x = (e.offsetX * this.dimensions[0]) / this.svg.getBoundingClientRect().width; // Handle canvas resize
-  const y = (e.offsetY * this.dimensions[1]) / this.svg.getBoundingClientRect().height;
+  const x = e.offsetX;
+  const y = e.offsetY;
 
   return [x, y]; // In graph coordinates
 }
@@ -14,8 +14,8 @@ export function _getTouchCoords(
   this: RealDrawBoard,
   touch: Touch
 ): [number, number] {
-  const x = ((touch.clientX - this.svg.getBoundingClientRect().left) * this.dimensions[0]) / this.svg.getBoundingClientRect().width; // Handle canvas resize
-  const y = ((touch.clientY - this.svg.getBoundingClientRect().top) * this.dimensions[1]) / this.svg.getBoundingClientRect().height;
+  const x = (touch.clientX - this.svg.getBoundingClientRect().left);
+  const y = (touch.clientY - this.svg.getBoundingClientRect().top);
 
   return [x, y];
 }
