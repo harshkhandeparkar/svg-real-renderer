25a2f709969f89365d39a4f393a50910823a7d65
HarshKhandeparkar <shadowwarriorpro3003@gmail.com>
2021-08-01 12:44:02 +0530

    fix: strore innerNodes in GroupNode

--- src/renderers/RealRenderer/strokeNodes/_group.ts
+++ src/renderers/RealRenderer/strokeNodes/_group.ts
@@ -2,6 +2,7 @@ import { StrokeNode, SVGSection } from '../../../types/RealRendererTypes';
 import { Node } from './_node';
 
 export class GroupNode extends Node<SVGGElement, 'group'> {
+  innerNodes: StrokeNode[] = [];
   constructor(
     section: SVGSection,
     initialInnerNodes: StrokeNode[]
@@ -10,6 +11,7 @@ export class GroupNode extends Node<SVGGElement, 'group'> {
     const g: SVGGElement = document.createElementNS('http://www.w3.org/2000/svg', 'g');
 
     initialInnerNodes.forEach((node) => g.appendChild(node.node));
+    this.innerNodes = initialInnerNodes;
 
     this.node = g;
     this.section = section;
